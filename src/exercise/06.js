ad2d0cc95cafd04f1a6652a70e14d40354adee5d
Chen Jie <e0036922@u.nus.edu>
2021-07-25 14:51:24 +0800

    intial state be null instead of boolean

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -20,7 +20,7 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   const inputRef = React.useRef()
-  const [error, setError] = React.useState(false)
+  const [error, setError] = React.useState(null)
 
   const handleSubmit = event => {
     event.preventDefault()
